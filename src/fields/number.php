3be74a426487fceff4f7d10b74d7bab1a07f102a
Mikel <mikel@pom.es>
2021-07-06 15:45:47 +0200

    New field type: number.

---
+++ src/fields/number.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace POM\Form;
+
+class Number {
+
+    public static function render_field(array $args): void {
+
+        echo '<div class="form-group">';
+
+        if (!empty($args['label'])) {
+            echo '<label for="' . $args['id'] . '">' . $args['label'] . '</label>';
+        }
+
+        if (!empty($args['description']) && $args['description_position'] === 'below_label') {
+            echo '<small class="description form-text text-muted">' . $args['description'] . '</small>';
+        }
+
+        ?>
+
+        <input type="number" id="<?= $args['id'] ?>" name="<?= $args['name'] ?>" value="<?= $args['value'] ?>" class="form-control <?= $args['class'] ?? '' ?>" <?= $args['custom_attrs'] ?>>
+
+        <?php
+
+        if (!empty($args['description']) && $args['description_position'] === 'under_field') {
+            echo '<small class="description form-text text-muted">' . $args['description'] . '</small>';
+        }
+
+        echo '</div>';
+
+    }
+
+}
