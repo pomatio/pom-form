8dbb8b42e690b7128f7c12cb9fcc1414bd8b24dc
Mikel <mikel@pom.es>
2022-05-31 14:33:28 +0200

    Updated repeater JS.

--- src/dist/js/repeater.js
+++ src/dist/js/repeater.js
@@ -40,7 +40,40 @@ jQuery(function($) {
     $(document).on('click', '.repeater-wrapper .delete', function(e) {
         e.preventDefault();
 
-        $(this).closest('.repeater').remove();
+        let $this = $(this);
+
+        let $wrapper = $this.closest('.repeater-wrapper');
+        $wrapper.find('.repeater-value').val('');
+        $this.closest('.repeater').remove();
+        $update_repeater($wrapper);
     });
+
+    /**
+     * Update repeater value
+     */
+    $(document).on('change', '.repeater-wrapper input, .repeater-wrapper textarea, .repeater-wrapper select', function() {
+        let $this = $(this);
+        $update_repeater($this.closest('.repeater-wrapper'));
+    });
+
+    let $update_repeater = function($wrapper) {
+        let $repeater_elements = $wrapper.find('.repeater');
+
+        let $value = [];
+
+        for (let $i = 0; $i < $repeater_elements.length; $i++) {
+            let $repeater_fields = $repeater_elements[$i].querySelectorAll("input, select, checkbox, textarea");
+            let $obj = {};
+
+            for (let $i2 = 0; $i2 < $repeater_fields.length; $i2++) {
+                let $field_name = $repeater_fields[$i2].getAttribute("name");
+                $obj[$field_name] = $repeater_fields[$i2].value;
+            }
+
+            $value.push($obj);
+        }
+
+        $wrapper.find('.repeater-value').val(JSON.stringify($value));
+    }
 
 });
