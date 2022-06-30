ab4837102d348424f399dbde347dbd1ad38b49f1
Mikel <mikel@pom.es>
2022-06-30 17:17:00 +0200

    Changes in the JS of the code editors to work with repeaters.

--- src/dist/js/code.js
+++ src/dist/js/code.js
@@ -1,40 +1,57 @@
+/**
+ * @var settings // Code editor settings object.
+ */
+
 jQuery(function($) {
-    $('textarea.form-control.pom-form-code-editor-css').each(function() {
-        let $this = $(this);
 
-        settings.codeMirrorSettings.codemirror.mode = 'text/css';
-        let $editor = wp.codeEditor.initialize($this, settings.codeMirrorSettings);
+    let $render_code_editor = function() {
+        $('textarea.form-control.pom-form-code-editor-css').each(function() {
+            let $this = $(this);
 
-        // Whenever something changes, update the main textarea so there's no issues when saving
-        $editor.codemirror.on('change', function(cm, change) {
-            cm.save();
-            $this.trigger('change');
+            settings.codeMirrorSettings.codemirror.mode = 'text/css';
+            let $editor = wp.codeEditor.initialize($this, settings.codeMirrorSettings);
+
+            // Whenever something changes, update the main textarea so there's no issues when saving
+            $editor.codemirror.on('change', function(cm, change) {
+                cm.save();
+                $this.trigger('change');
+            });
         });
+
+        $('textarea.form-control.pom-form-code-editor-js').each(function() {
+            let $this = $(this);
+
+            settings.codeMirrorSettings.codemirror.mode = 'application/javascript';
+            let $editor = wp.codeEditor.initialize($this, settings.codeMirrorSettings);
+
+            // Whenever something changes, update the main textarea so there's no issues when saving
+            $editor.codemirror.on('change', function(cm, change) {
+                cm.save();
+                $this.trigger('change');
+            });
+        });
+
+        $('textarea.form-control.pom-form-code-editor-html').each(function() {
+            let $this = $(this);
+
+            settings.codeMirrorSettings.codemirror.mode = 'text/html';
+            let $editor = wp.codeEditor.initialize($this, settings.codeMirrorSettings);
+
+            // Whenever something changes, update the main textarea so there's no issues when saving
+            $editor.codemirror.on('change', function(cm, change) {
+                cm.save();
+                $this.trigger('change');
+            });
+        });
+    }
+
+    $render_code_editor();
+
+    /**
+     * Adjustment so that code editors added by repeaters render correctly.
+     */
+    $(document).ajaxComplete(function() {
+        $render_code_editor();
     });
 
-    $('textarea.form-control.pom-form-code-editor-js').each(function() {
-        let $this = $(this);
-
-        settings.codeMirrorSettings.codemirror.mode = 'application/javascript';
-        let $editor = wp.codeEditor.initialize($this, settings.codeMirrorSettings);
-
-        // Whenever something changes, update the main textarea so there's no issues when saving
-        $editor.codemirror.on('change', function(cm, change) {
-            cm.save();
-            $this.trigger('change');
-        });
-    });
-
-    $('textarea.form-control.pom-form-code-editor-html').each(function() {
-        let $this = $(this);
-
-        settings.codeMirrorSettings.codemirror.mode = 'text/html';
-        let $editor = wp.codeEditor.initialize($this, settings.codeMirrorSettings);
-
-        // Whenever something changes, update the main textarea so there's no issues when saving
-        $editor.codemirror.on('change', function(cm, change) {
-            cm.save();
-            $this.trigger('change');
-        });
-    });
 });
