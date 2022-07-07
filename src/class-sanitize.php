c7879e3f967b847d3e31c662924e95381cba5be1
Mikel <mikel@pom.es>
2022-07-07 11:49:01 +0200

    Improvements to sanitize_pom_form_repeater() method.

--- src/class-sanitize.php
+++ src/class-sanitize.php
@@ -3,12 +3,20 @@
  * Functions to ensure that values are safe to tamper with.
  */
 
-namespace POM\Form;
+use POM\Form\POM_Form_Disk;
+use POM\Form\POM_Form_Helper;
 
 function sanitize_pom_form_button($value) {
     return $value;
 }
 
+/**
+ * Returns 'yes' or 'no' if it is a single checkbox.
+ * If it is an array of checkboxes, it returns a value in array format.
+ *
+ * @param $value
+ * @return array|mixed|string
+ */
 function sanitize_pom_form_checkbox($value) {
     if (is_array($value)) {
         return !empty($value) ? $value : [];
@@ -20,7 +28,13 @@ function sanitize_pom_form_code_css($value) {
     return $value;
 }
 
-function sanitize_pom_form_code_html($value) {
+/**
+ * Filters text content and strips out disallowed HTML.
+ *
+ * @param $value
+ * @return string
+ */
+function sanitize_pom_form_code_html($value): string {
     $allowed_tags = POM_Form_Helper::get_allowed_html();
     return wp_kses($value, $allowed_tags, []);
 }
@@ -46,7 +60,7 @@ function sanitize_pom_form_datetime($value) {
     return $value;
 }
 
-function sanitize_pom_form_email($value) {
+function sanitize_pom_form_email($value): string {
     return sanitize_email($value);
 }
 
@@ -54,20 +68,39 @@ function sanitize_pom_form_file($value) {
     return $value;
 }
 
+/**
+ * Allow only numbers (from 0 to 9) and comma (,).
+ *
+ * @param $value
+ * @return false|mixed
+ */
 function sanitize_pom_form_gallery($value) {
-    return $value;
+    $validate = preg_match("/^[0-9,]+$/", $value);
+    return $validate ? $value : false;
 }
 
-function sanitize_pom_form_hidden($value) {
+function sanitize_pom_form_hidden($value): string {
     return sanitize_text_field($value);
 }
 
-function sanitize_pom_form_icon_picker($value) {
-    return $value;
+/**
+ * Validate that it is a real URL.
+ *
+ * @param $value
+ * @return string
+ */
+function sanitize_pom_form_icon_picker($value): string {
+    return sanitize_url($value);
 }
 
-function sanitize_pom_form_image_picker($value) {
-    return $value;
+/**
+ * Validate that it is a real URL.
+ *
+ * @param $value
+ * @return string
+ */
+function sanitize_pom_form_image_picker($value): string {
+    return sanitize_url($value);
 }
 
 function sanitize_pom_form_number($value) {
@@ -82,7 +115,7 @@ function sanitize_pom_form_quantity($value) {
     return filter_var($value, FILTER_SANITIZE_NUMBER_FLOAT);
 }
 
-function sanitize_pom_form_radio($value) {
+function sanitize_pom_form_radio($value): string {
     return sanitize_text_field($value);
 }
 
@@ -90,11 +123,52 @@ function sanitize_pom_form_range($value) {
     return filter_var($value, FILTER_SANITIZE_NUMBER_FLOAT);
 }
 
-function sanitize_pom_form_repeater($value) {
+function sanitize_pom_form_repeater($value, $array_settings = [], $settings_dir = 'pom-form'): array {
+    if (!empty($value)) {
+        $sanitized_array = [];
+
+        $limit = isset($array_settings['limit']) ? (int)$array_settings['limit'] : '';
+
+        $i = 0;
+        foreach ($value as $type => $items) {
+            foreach ($items as $index => $arr_data) {
+                if (!empty($limit) && $limit === $i++) {
+                    break;
+                }
+
+                $repeater_identifier = '';
+                foreach ($arr_data as $arr_key => $arr_value) {
+                    if ($arr_key === 'repeater_identifier') {
+                        $sanitized_array[$type][$index][$arr_key] = $repeater_identifier = sanitize_pom_form_text($arr_value);
+                    }
+                    elseif ($arr_key === 'default_values') {
+                        $sanitized_array[$type][$index][$arr_key] = json_decode(stripslashes($arr_value), true);
+                    }
+                    else {
+                        $sanitize_function_name = "sanitize_pom_form_{$arr_value['type']}";
+
+                        if (isset($array_settings['name']) && ($arr_value['type'] === 'code_html' || $arr_value['type'] === 'code_css' || $arr_value['type'] === 'code_js')) {
+                            $file_name = "{$array_settings['name']}_{$repeater_identifier}_{$arr_key}";
+                            $sanitized_array[$type][$index][$arr_key]['value'] = POM_Form_Disk::save_to_file($file_name, $arr_value['value'], str_replace('code_', '', $arr_value['type']), $settings_dir);
+
+                        }
+                        else {
+                            $sanitized_array[$type][$index][$arr_key]['value'] = $sanitize_function_name($arr_value['value']);
+                        }
+
+                        $sanitized_array[$type][$index][$arr_key]['type'] = $arr_value['type'];
+                    }
+                }
+            }
+        }
+
+        return $sanitized_array;
+    }
+
     return $value;
 }
 
-function sanitize_pom_form_select($value) {
+function sanitize_pom_form_select($value): string {
     return sanitize_text_field($value);
 }
 
@@ -110,11 +184,11 @@ function sanitize_pom_form_tel($value) {
     return $validate ? $value : false;
 }
 
-function sanitize_pom_form_text($value) {
+function sanitize_pom_form_text($value): string {
     return sanitize_text_field($value);
 }
 
-function sanitize_pom_form_textarea($value) {
+function sanitize_pom_form_textarea($value): string {
     return sanitize_textarea_field($value);
 }
 
