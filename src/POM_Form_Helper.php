18eaf05da5da14c26a72ae4c09f4252052000b02
Mikel <mikel@pom.es>
2022-09-30 13:45:53 +0200

    Updated POM_Form_Helper get_path method.

--- src/POM_Form_Helper.php
+++ src/POM_Form_Helper.php
@@ -4,25 +4,8 @@ namespace PomatioFramework;
 
 class POM_Form_Helper {
 
-    /**
-     * This function will return true if the current file
-     * is loaded in a plugin and false if it is loaded in a theme.
-     *
-     * https://wordpress.stackexchange.com/a/213044
-     *
-     * @return bool
-     */
-    public static function is_plugin(): bool {
-        return strpos( str_replace("\\", "/", plugin_dir_path( __FILE__ ) ) , str_replace("\\", "/", WP_PLUGIN_DIR) ) !== false;
-    }
-
     public static function get_path(): string {
-        return self::is_plugin() ? plugin_dir_path(__FILE__) : get_template_directory();
-    }
-
-    public static function get_uri($file = ''): string {
-        $file = !empty($file) ? $file : __FILE__;
-        return self::is_plugin() ? plugin_dir_url($file) : get_template_directory_uri();
+        return __DIR__;
     }
 
     /**
