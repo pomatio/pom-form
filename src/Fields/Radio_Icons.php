39adb5b292ca499a333820fb1caf8589f200ba7e
Mikel <mikel@pom.es>
2022-11-21 14:54:54 +0100

    Improved radio-icons field.

--- src/Fields/Radio_Icons.php
+++ src/Fields/Radio_Icons.php
@@ -21,7 +21,7 @@ class Radio_Icons {
             <?php
 
             foreach ($args['options'] as $radio_value => $radio_label) {
-                if (wp_http_validate_url($radio_label)) {
+                if (!wp_http_validate_url($radio_label)) {
                     continue;
                 }
 
@@ -29,10 +29,9 @@ class Radio_Icons {
 
                 ?>
 
-                <label class="form-check">
+                <label class="icon-wrapper">
                     <input type="radio" id="<?= $args['name'] . '-' . $radio_value ?>" name="<?= $args['name'] ?>" value="<?= $radio_value ?>" class="form-check-input form-control <?= $args['class'] ?? '' ?>" <?= $checked ?> data-type="radio">
                     <span class="icon"><img src="<?= $radio_label ?>" alt="<?= $radio_label ?>"></span>
-                    <span class="form-check-label"><?= $radio_label ?></span>
                 </label>
 
                 <?php
