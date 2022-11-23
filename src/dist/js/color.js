3c163dbd8ef7d293c993fbe56b44caf5e9041a86
Mikel <mikel@pom.es>
2022-11-23 08:32:12 +0100

    Added color.js file.

---
+++ src/dist/js/color.js
@@ -0,0 +1,9 @@
+jQuery(function($) {
+    if (typeof $.fn.wpColorPicker !== 'undefined') {
+        $('.pomatio-framework-color-picker').wpColorPicker();
+
+        $(document).ajaxComplete(function() {
+            $('.pomatio-framework-color-picker').wpColorPicker();
+        });
+    }
+});
