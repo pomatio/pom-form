2c1967047ca3a53c07166b4fe9fd9670bbf400eb
Mikel <mikel@pom.es>
2022-11-23 10:14:34 +0100

    New separator field.

---
+++ src/Fields/Separator.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace PomatioFramework\Fields;
+
+class Separator {
+
+    public static function render_field(array $args): void {
+        if (isset($args['label']) && !empty($args['label'])) {
+            echo "<h2>{$args['label']}</h2>";
+        }
+
+        if (isset($args['description']) && !empty($args['description'])) {
+            echo '<p class="description form-text text-muted">' . $args['description'] . '</p>';
+        }
+    }
+
+}
