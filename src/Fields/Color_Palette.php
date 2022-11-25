640cb3ced4eafb78d985836be08e70b30e9cd4a6
Mikel <mikel@pom.es>
2022-11-25 11:12:01 +0100

    Updated Color_Palette field.

--- src/Fields/Color_Palette.php
+++ src/Fields/Color_Palette.php
@@ -22,14 +22,29 @@ class Color_Palette {
         if (!empty($colors)) {
             $i = 0;
 
-            foreach ($colors as $color_slug => $hex) {
+            foreach ($colors as $color_slug => $color_data) {
                 $checked = checked((string)$args['value'], (string)$color_slug, false);
 
                 ?>
 
                 <input type="radio" name="<?= $args['name'] ?>" id="<?= $args['name'] . '_' . $i ?>" value="<?= $color_slug ?>" <?= $checked ?>>
-                <label style="background-color: <?= $hex ?? '#f3f3f3' ?>" for="<?= $args['name'] . '_' . $i ?>">
-                    <span class="name"><?= $color_slug ?></span>
+                <label style="background-color: <?= $color_data['hex'] ?? '#f3f3f3' ?>" for="<?= $args['name'] . '_' . $i ?>">
+
+                    <?php
+
+                    if (isset($color_data['icon']) && file_exists($color_data['icon'])) {
+                        $icon = file_get_contents($color_data['icon']);
+
+                        ?>
+
+                        <span class="icon"><?= $icon ?></span>
+
+                        <?php
+                    }
+
+                    ?>
+
+                    <span class="name"><?= $color_data['label'] ?? $color_slug ?></span>
                 </label>
 
                 <?php
