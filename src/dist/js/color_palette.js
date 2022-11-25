f81f87a4add6d42b47aba50a03225ea6259d2467
Mikel <mikel@pom.es>
2022-11-25 12:18:08 +0100

    Minor fix.

--- src/dist/js/color_palette.js
+++ src/dist/js/color_palette.js
@@ -2,6 +2,6 @@ jQuery(function($) {
     $(document).on('click', '.restore-color-palette', function() {
         let $this = $(this);
         let $default = $this.attr('data-default');
-        $this.closest('.color-palette-wrapper').find('input[value="' + $default + '"]').attr('checked', 'checked');
+        $this.closest('.color-palette-wrapper').find('input[value="' + $default + '"]').prop('checked', 'checked');
     });
 });
