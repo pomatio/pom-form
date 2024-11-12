b959edeb0059a2fb67437152fdb8368010c954a7
Mikel <mikel@pom.es>
2024-11-12 12:28:44 +0100

    Empty the hidden field of the signature field when deleting the Canvas content.

--- src/dist/js/signature.js
+++ src/dist/js/signature.js
@@ -2,22 +2,22 @@ const canvases = document.querySelectorAll('.signature-canvas');
 
 canvases.forEach(canvas => {
   const ctx = canvas.getContext('2d');
-  
+
   // Set canvas dimensions to match its natural resolution
   canvas.width = canvas.offsetWidth;
   canvas.height = canvas.offsetHeight;
-  
+
   let isDrawing = false;
   let lastX = 0;
   let lastY = 0;
-  
+
   canvas.addEventListener('mousedown', function(e) {
     isDrawing = true;
     let rect = canvas.getBoundingClientRect();
     lastX = e.clientX - rect.left;
     lastY = e.clientY - rect.top;
   });
-  
+
   canvas.addEventListener('mousemove', function(e) {
     if (isDrawing) {
       let rect = canvas.getBoundingClientRect();
@@ -28,12 +28,12 @@ canvases.forEach(canvas => {
       lastY = mouseY;
     }
   });
-  
+
   canvas.addEventListener('mouseup', function() {
     isDrawing = false;
     saveCanvasData(canvas);
   });
-  
+
   canvas.addEventListener('mouseout', function() {
     isDrawing = false;
   });
@@ -54,16 +54,19 @@ const clearButtons = document.querySelectorAll('.signature-canvas-clear');
 clearButtons.forEach(button => {
   button.addEventListener('click', function(e) {
     e.preventDefault();
-    
+
     const canvas = this.parentElement.querySelector('canvas');
     const ctx = canvas.getContext('2d');
-    
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
-    saveCanvasData(canvas);
+
+    saveCanvasData(canvas, true);
   });
 });
 
-function saveCanvasData(canvas) {
-  canvas.parentElement.querySelector('.signature-value').value = canvas.toDataURL();
+function saveCanvasData(canvas, clear = false) {
+  const input = canvas.parentElement.querySelector('.signature-value');
+  input.value = clear ? '' : canvas.toDataURL();
+
+  input.dispatchEvent(new Event('change', { bubbles: true }));
 }
