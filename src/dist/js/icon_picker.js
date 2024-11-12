a8b4e5160e693158917405a731ffe8a6e5afcdcd
Mikel <mikel@pom.es>
2024-11-12 17:18:05 +0100

    Remove trailing whitespace in icon_picker.js.

--- src/dist/js/icon_picker.js
+++ src/dist/js/icon_picker.js
@@ -1,21 +1,21 @@
 jQuery(function($) {
-  
+
   if (typeof $.fn.dialog === 'undefined') {
     return;
   }
-  
+
   // Display remove button only if icon is selected.
   $('.icon-picker-wrapper .icon-wrapper').each(function() {
     let $this = $(this);
-    
+
     if ($this.find('img').length) {
       $(this).closest('.icon-picker-wrapper').find('.remove-selected-icon').css('display', 'inherit');
     }
   });
-  
+
   // Store the icon picker button that has been clicked
   let $clicked_button;
-  
+
   let $icon_picker_modal = $('#pomatio-framework-icons-modal').dialog({
     title: '',
     dialogClass: 'wp-dialog pomatio-framework-icons-modal',
@@ -40,27 +40,27 @@ jQuery(function($) {
       // Set defaults to all libraries on modal close.
       $('#pomatio-framework-icons-modal .media-menu-item').removeClass('active').attr('aria-selected', '');
       $('#pomatio-framework-icons-modal .media-frame-content').empty().append('<span class="centered-text">' + pom_form_icon_picker.loading + '</span>');
-      
+
       $('.pomatio-framework-icons-modal .media-menu [data-slug="all"]').addClass('active').attr('aria-selected', 'true');
       let $title = $('.pomatio-framework-icons-modal [data-slug="all"]').attr('data-label');
       $('#pomatio-framework-icons-modal .media-frame-title').empty().append('<h1>' + $title + '</h1>');
     },
     create: function() {
       $('.pomatio-framework-icons-modal .ui-dialog-titlebar').css('display', 'none');
-      
+
       // Set defaults to all libraries on modal creation.
       $('.pomatio-framework-icons-modal .media-menu [data-slug="all"]').addClass('active').attr('aria-selected', 'true');
       let $title = $('.pomatio-framework-icons-modal [data-slug="all"]').attr('data-label');
       $('#pomatio-framework-icons-modal .media-frame-title').empty().append('<h1>' + $title + '</h1>');
     }
   });
-  
+
   $(document).on('click', '.open-icon-picker-modal', function(e) {
     e.preventDefault();
-    
+
     $clicked_button = $(this);
     $icon_picker_modal.dialog('open');
-    
+
     $.ajax({
       url: ajaxurl,
       method: 'POST',
@@ -73,29 +73,29 @@ jQuery(function($) {
       }
     });
   });
-  
+
   $(document).on('click', '.close-icon-picker-modal', function(e) {
     e.preventDefault();
     $icon_picker_modal.dialog('close');
   });
-  
+
   /**
    * Get the icons of selected library.
    */
   $(document).on('click', '#pomatio-framework-icons-modal .media-menu-item', function() {
     let $this = $(this);
-    
+
     $('#pomatio-framework-icons-modal input[type="search"]').val('');
-    
+
     $this.closest('#pomatio-framework-icons-modal').find('.media-frame-content').empty().append('<span class="centered-text">' + pom_form_icon_picker.loading + '</span>');
-    
+
     $('#pomatio-framework-icons-modal .media-menu-item').removeClass('active').attr('aria-selected', '');
     $this.addClass('active').attr('aria-selected', 'true');
-    
+
     let $library = $this.attr('data-slug');
     let $label = $this.attr('data-label');
     $this.closest('#pomatio-framework-icons-modal').find('.media-frame-title').empty().append('<h1>' + $label + '</h1>');
-    
+
     $.ajax({
       url: ajaxurl,
       method: 'POST',
@@ -108,7 +108,7 @@ jQuery(function($) {
       }
     });
   });
-  
+
   /**
    * When clicking on the icon.
    */
@@ -117,19 +117,19 @@ jQuery(function($) {
     $(this).addClass('selected');
     $('#pomatio-framework-icons-modal .pomatio-framework-icon-select-button').removeClass('disabled').prop('disabled', false);
   });
-  
+
   /**
    * Pagination. Load more icons.
    */
   $(document).on('click', '#pomatio-framework-icons-modal .load-more-icons button', function() {
     let $this = $(this);
-    
+
     $this.hide();
     $this.next('.icon-picker-spinner').show();
-    
+
     let $offset = parseInt($this.attr('data-offset'));
     let $library = $('#pomatio-framework-icons-modal .media-menu-item.active').attr('data-slug');
-    
+
     $.ajax({
       url: ajaxurl,
       method: 'POST',
@@ -145,21 +145,21 @@ jQuery(function($) {
       }
     });
   });
-  
+
   /**
    * On Select icon button click.
    */
   $(document).on('click', '#pomatio-framework-icons-modal .pomatio-framework-icon-select-button', function(e) {
     e.preventDefault();
-    
+
     let $icon_url = $('#pomatio-framework-icons-modal .attachment.selected img').attr('src');
     $clicked_button.closest('.icon-picker-wrapper').find('input[type="hidden"]').val($icon_url);
     $clicked_button.closest('.icon-picker-wrapper').find('.icon-wrapper').empty().append('<img alt="" src="' + $icon_url + '">');
     $clicked_button.closest('.icon-picker-wrapper').find('.remove-selected-icon').css('display', 'inherit');
-    
+
     $icon_picker_modal.dialog('close');
   });
-  
+
   /**
    * The delay function will return a wrapped function that internally handles an individual timer,
    * in each execution the timer is restarted with the time delay provided,
@@ -177,13 +177,13 @@ jQuery(function($) {
       timer = setTimeout(fn.bind(this, ...args), ms || 0);
     };
   }
-  
+
   $(document).on('keyup', '#pomatio-framework-icons-modal input[type="search"]', delay_search(function() {
     let $this = $(this);
     let $search = $this.val();
     let $library = $this.closest('#pomatio-framework-icons-modal').find('.media-menu button.active').attr('data-slug');
     $this.closest('#pomatio-framework-icons-modal').find('.media-frame-content').empty().append('<span class="centered-text">' + pom_form_icon_picker.loading + '</span>');
-    
+
     $.ajax({
       url: ajaxurl,
       method: 'POST',
@@ -197,16 +197,16 @@ jQuery(function($) {
       }
     });
   }, 500));
-  
+
   $(document).on('click', '.icon-picker-wrapper .remove-selected-icon', function(e) {
     e.preventDefault();
-    
+
     let $this = $(this);
     let $wrapper = $this.closest('.icon-picker-wrapper');
-    
+
     $wrapper.find('.icon-wrapper').empty();
     $wrapper.find('input[type="hidden"]').val('');
     $this.css('display', 'none');
   });
-  
+
 });
