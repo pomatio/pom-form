b65ddafa24dafe01325e6c265e25872785113a32
Mikel <mikel@pom.es>
2025-06-04 12:43:32 +0200

    Update 'enable_theme_option' CLI command.

--- src/cli/commands/enable-theme-option.php
+++ src/cli/commands/enable-theme-option.php
@@ -20,11 +20,19 @@ if (defined('WP_CLI') && WP_CLI) {
          * @throws ExitException
          */
         public function __invoke($args, $assoc_args): void {
+            if (empty($assoc_args['settings_dir'])) {
+                WP_CLI::error("The --settings_dir=<string> argument is required.");
+            }
+
+            if (empty($assoc_args['section'])) {
+                WP_CLI::error("The --section=<string> argument is required.");
+            }
+
             Pomatio_Framework_Disk::create_settings_dir($assoc_args['settings_dir']);
 
             $site = '';
 
-            if (isset($assoc_args['url'])) {
+            if (!empty($assoc_args['url'])) {
                 $site = ' for ' . $args['url'];
             }
 
@@ -34,17 +42,15 @@ if (defined('WP_CLI') && WP_CLI) {
              * 0 = Disabled
              */
             $settings_array = array_filter((array)Pomatio_Framework_Disk::read_file('enabled_settings.php', $assoc_args['settings_dir'], 'array'));
-
-            if (isset($settings_array[$assoc_args['section']])) {
-                WP_CLI::error("The setting is already defined$site.");
-            }
-
             $settings_array[$assoc_args['section']] = '1';
 
-            $settings_content = (new Pomatio_Framework_Disk)->generate_file_content($settings_array, "Enabled settings array file.");
-            $settings_path = (new Pomatio_Framework_Disk())->get_settings_path($assoc_args['settings_dir']);
+            $disk_instance = new Pomatio_Framework_Disk();
+            $settings_content = $disk_instance->generate_file_content($settings_array, "Enabled settings array file.");
+            $settings_path = $disk_instance->get_settings_path($assoc_args['settings_dir']);
 
-            file_put_contents($settings_path . 'enabled_settings.php', $settings_content, LOCK_EX);
+            if (false === file_put_contents($settings_path . 'enabled_settings.php', $settings_content, LOCK_EX)) {
+                WP_CLI::error("Failed to write to settings file: " . $settings_path . 'enabled_settings.php');
+            }
 
             WP_CLI::success("{$assoc_args['section']} setting enabled$site.");
         }
