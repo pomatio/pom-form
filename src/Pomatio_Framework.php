303c7ff21ac62094688c41eb5201d1e68dcbd65e
Germán Oronoz <german@pom.es>
2026-01-12 19:11:24 +0100

    Added wp_enqueue_media() when a repeater contains a Font_Picker, so the media frame scripts load on the initial page render and wp.media is defined even before the first repeater item is saved.

--- src/Pomatio_Framework.php
+++ src/Pomatio_Framework.php
@@ -146,6 +146,7 @@ class Pomatio_Framework {
                     );
                     $mime_types = array_values(array_unique($mime_types));
 
+                    wp_enqueue_media();
                     wp_enqueue_style('pomatio-framework-font_picker', POM_FORM_SRC_URI . '/dist/css/font-picker.min.css');
                     wp_enqueue_script('pomatio-framework-font_picker', POM_FORM_SRC_URI . '/dist/js/font_picker' . POMATIO_MIN . '.js', ['jquery'], null, true);
                     wp_localize_script(
